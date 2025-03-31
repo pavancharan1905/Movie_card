f5fdf770313ad683475539dbde3a72ea99a07726
pavancharan1905 <charanprasad1234@gmail.com>
2025-03-31 23:10:03 +0530

    New

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App(){
   return(
     <Provider store={store}>
       <div>
-        <h1>App</h1>
         <Home />
       </div>
     </Provider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,3 +48,4 @@ const gridStyle = {
 }; 
 
 export default Home; 
+
