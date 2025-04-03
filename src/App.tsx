91b6e6af0b1a5804c69231235e541e5d706e3ece
pavancharan1905 <charanprasad1234@gmail.com>
2025-04-03 15:53:02 +0530

    Additional feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CssBaseline, Box } from '@mui/material';
 import { Provider } from 'react-redux';
-import Navbar from './component/NavBar';
+
+import Navbar from './component/NavBar'; 
 import { store } from './redux/store';
 import Home from './pages/Home';
 import Search from './pages/Search';
-import { Box } from '@mui/material';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import WatchList from './pages/Watchlist';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <Provider store={store}>
-     
-      
-        <Router>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline /> {}
+        <BrowserRouter>
           <Navbar />
           <Box sx={{ mt: 8 }}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/watchlist" element={<WatchList />} />
             </Routes>
           </Box>
-        </Router>
-     
-    </Provider>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
